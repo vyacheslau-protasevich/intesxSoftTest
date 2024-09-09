12655b8401b20b24713e081c0724717b64f421a6
slava <vyacheslauprot@gmail.com>
2024-09-09 13:02:36 +0300

    changed order of endpoints + small fixes in Exceptions

--- src/api/routes/user.py
+++ src/api/routes/user.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from exceptions.user import UserDoesNotExistError
+from exceptions.user import UserDoesNotExistError, NoUpdateDataProvidedError
 from fastapi import APIRouter, Depends, Form, Response, HTTPException
 from services.db.db import Neo4jService
 from starlette import status
@@ -22,6 +22,13 @@ def create_user(
     return neo4j_service.create_user(user)
 
 
+@router.get("/all_users", status_code=status.HTTP_200_OK)
+def get_all_users(
+        neo4j_service: Neo4jService = Depends(get_neo4j_sevice),
+):
+    return neo4j_service.get_all_users()
+
+
 @router.get("/{user_id}", status_code=status.HTTP_200_OK, summary="Get user by id")
 def get_user_profile(
         user_id: UUID,
@@ -29,15 +36,8 @@ def get_user_profile(
 ):
     try:
         return neo4j_service.get_user_by_id(user_id)
-    except UserDoesNotExistError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-
-
-@router.get("/all_users", status_code=status.HTTP_200_OK)
-def get_all_users(
-        neo4j_service: Neo4jService = Depends(get_neo4j_sevice),
-):
-    return neo4j_service.get_all_users()
+    except UserDoesNotExistError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @router.put("/{user_id}", status_code=status.HTTP_202_ACCEPTED)
@@ -48,7 +48,7 @@ def update_user(
         phone: int | None = Form(None),
         address: str | None = Form(None),
         city: str | None = Form(None),
-        state: str | None = Form(None),
+        state: str | None = Form(None, min_length=2, max_length=2),
         zipcode: int | None = Form(None),
         profile_photo: str | None = Form(None),
         neo4j_service: Neo4jService = Depends(get_neo4j_sevice),
@@ -76,8 +76,10 @@ def update_user(
     )
     try:
         neo4j_service.update_user_by_id(user_id, update_data)
-    except UserDoesNotExistError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except UserDoesNotExistError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    except NoUpdateDataProvidedError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     return Response(status_code=status.HTTP_200_OK)
 
 
@@ -88,6 +90,8 @@ def delete_user(
 ):
     try:
         neo4j_service.delete_user_by_id(user_id)
-    except UserDoesNotExistError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except UserDoesNotExistError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
