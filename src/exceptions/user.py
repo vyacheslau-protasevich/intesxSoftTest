13b2b3a0140335043ae2e93cb693c668754e47df
slava <vyacheslauprot@gmail.com>
2024-09-09 14:10:25 +0300

    added exceptions for creating random profiles and friends

--- src/exceptions/user.py
+++ src/exceptions/user.py
@@ -20,3 +20,15 @@ class UsersAreAlreadyFriendsError(Exception):
 
     def __init__(self, user_id_1: str, user_id_2: str) -> None:
         super().__init__(f"Users with id='{user_id_1}' and id='{user_id_2}' are already friends")
+
+
+class CannotMakeFriendsWithSelfError(Exception):
+
+    def __init__(self) -> None:
+        super().__init__("Cannot make friends with self")
+
+
+class ToManyFriendsToCreateError(Exception):
+
+    def __init__(self) -> None:
+        super().__init__("Cannot make friends with such amount of users")
