6c1f131c7abe776c6f2fdf7698bd75ad7ce461a6
Viachaslau Pratasevich <slavaarrow@gmail.com>
2024-09-09 11:21:57 +0300

    added new exceptions for user

--- src/exceptions/user.py
+++ src/exceptions/user.py
@@ -2,3 +2,9 @@ class UserDoesNotExistError(Exception):
 
     def __init__(self, id: str) -> None:
         super().__init__(f"User with id='{id}' does not exist")
+
+
+class NoUpdateDataProvidedError(Exception):
+
+    def __init__(self) -> None:
+        super().__init__("No update data provided")
