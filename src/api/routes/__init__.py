2a178758f173d9fb1a5f334f1b706ea882cc1dcd
slava <vyacheslauprot@gmail.com>
2024-09-09 13:03:07 +0300

    connected friends endpoints

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from api.routes import test
 from api.routes import user
+from api.routes import friends
 
 
 def setup_routes(app: FastAPI) -> None:
     app.include_router(test.router)
     app.include_router(user.router)
+    app.include_router(friends.router)
