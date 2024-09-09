bd246940c4ce5907f56fc709ce815eca88e08320
Viachaslau Pratasevich <slavaarrow@gmail.com>
2024-09-09 11:17:56 +0300

    removed cors middleware

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 from dotenv import find_dotenv
 
-from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,21 +10,11 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8"
     )
 
-    allow_origins: list[str] = ["*"]
-
-    # secret_key: str
-    # algorithm: str = "HS256"
-    # access_token_expires_seconds: int = 60 * 60 * 24 * 365
-
-    postgres_user: str
-    postgres_password: str
-    postgres_host: str
-    postgres_port: int
-    postgres_db: str
+    neo4j_user: str
+    neo4j_password: str
+    neo4j_host: str
+    neo4j_port: int
 
     @property
-    def postgres_dsn(self) -> PostgresDsn:
-        return (
-            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@"
-            f"{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
-        )
+    def neo4j_uri(self) -> str:
+        return f"bolt://{self.neo4j_host}:{self.neo4j_port}"
