f49af694204725da646f5ea0a04b9c6ab29c0583
slava <vyacheslauprot@gmail.com>
2024-09-10 14:24:48 +0300

    set up configuration for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,42 +4,32 @@ import uuid
 
 from httpx import AsyncClient
 import pytest
-from neo4j import GraphDatabase
 
 from src.config import Settings
 from src.main import app
+from neo4j import GraphDatabase
 
-
-@pytest.fixture(scope="session")
-async def created_book_id(session_maker: async_sessionmaker[AsyncSession]) -> str:
-    async with session_maker() as session:
-        result = await session.execute(text("SELECT books.book_id from books LIMIT 1;"))
-    return str(result.scalar_one())
-
-
-@pytest.fixture(scope="session")
-async def test_admin_user() -> AdminUserExample:
-    return AdminUserExample(
-        user_id=uuid.UUID("53bf51fa-08c3-4ace-be33-347f4504cc9c"),
-        name="Test user name",
-        device_id="13371488xxx",
-        email="test.user@gmail.com",
-        password="1",
-        hashed_password="$2b$12$ao91w17KXBX6gj6P3mkwI.dFTBTYzF4jjUgnLJt/alfaP.vGjWFqC",
-        completed_tutorial=True,
-        preferred_genres=["haha", "hehe", "hoho"],
-        role=UserRole.ADMIN,
-        gender=UserGender.MALE,
-        language="en",
-        registered_at=datetime.datetime(year=2024, month=1, day=1)
-    )
 
 @pytest.fixture(autouse=True, scope="session")
 def config() -> Settings:
     return Settings()
 
+
+@pytest.fixture(autouse=True, scope="session")
+async def neo4j_driver(config: Settings):
+    driver = GraphDatabase.driver(config.neo4j_uri, auth=(config.neo4j_user, config.neo4j_password))
+    yield driver
+    driver.close()
+
+
 @pytest.fixture(scope="session")
-async def session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
-    return async_sessionmaker(
-        bind=engine, expire_on_commit=False, autoflush=False
-    )
+async def async_client():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
