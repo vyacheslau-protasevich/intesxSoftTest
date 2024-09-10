9c877b5a73574bed6fbb09019a32f0ec1ed31871
slava <vyacheslauprot@gmail.com>
2024-09-10 18:44:49 +0300

    added user data for testing

---
+++ tests/entities.py
@@ -0,0 +1,40 @@
+from src.models.user import UserIn
+
+
+user_example_1 = UserIn(
+    first_name="John",
+    last_name="Doe",
+    phone=1234567890,
+    address="123 Main St",
+    city="Springfield",
+    state="IL",
+    zipcode=62701,
+    profile_photo="http://example.com/photo.jpg",
+    available=True
+
+)
+
+user_example_2 = UserIn(
+    first_name="Mile",
+    last_name="Hill",
+    phone=234234214,
+    address="121 Main St",
+    city="Springfield",
+    state="IO",
+    zipcode=34213,
+    profile_photo="http://example.com/photo.jpg",
+    available=True
+)
+
+user_example_3 = UserIn(
+    first_name="Jane",
+    last_name="Smith",
+    phone=1234567890,
+    address="123 Main St",
+    city="Springfield",
+    state="IL",
+    zipcode=62701,
+    profile_photo="http://example.com/photo.jpg",
+    available=True
+)
+
