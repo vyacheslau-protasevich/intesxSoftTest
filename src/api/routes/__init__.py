f552b813beeed75b5d8f158998f17e3f2223f10f
slava <vyacheslauprot@gmail.com>
2024-09-10 18:46:18 +0300

    removed testing route

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 
-from api.routes import test
 from api.routes import user
 from api.routes import friends
 from api.routes import random
 
 
 def setup_routes(app: FastAPI) -> None:
-    app.include_router(test.router)
     app.include_router(user.router)
     app.include_router(friends.router)
     app.include_router(random.router)
