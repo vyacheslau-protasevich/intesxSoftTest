b38317537af9ad39add3559e931a40b8d840c8ea
slava <vyacheslauprot@gmail.com>
2024-09-10 18:45:23 +0300

    implemented tests for users endpoints

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,26 +1,19 @@
 from uuid import uuid4
 
+from httpx import AsyncClient
 
-def test_create_user(client):
-    user_data = {
-        "first_name": "Jane",
-        "last_name": "Doe",
-        "phone": 1234567890,
-        "address": "123 Main St",
-        "city": "Springfield",
-        "state": "IL",
-        "zipcode": 62701,
-        "profile_photo": "http://example.com/photo.jpg"
-    }
-    response = client.post("/api/users", json=user_data)
+from tests.entities import user_example_1
+
+
+async def test_create_user(async_client: AsyncClient):
+    user = user_example_1
+    response = await async_client.post("/api/users", data=user.json())
     assert response.status_code == 201
     assert "id" in response.json()
-    assert response.json()["first_name"] == "Jane"
-    assert response.json()["last_name"] == "Doe"
 
 
-def test_get_all_users(client):
-    response = client.get("/api/users/all_users")
+async def test_get_all_users(async_client: AsyncClient):
+    response = await async_client.get("/api/users/all_users")
     assert response.status_code == 200
     users = response.json()
     assert isinstance(users, list)
@@ -29,26 +22,24 @@ def test_get_all_users(client):
     assert "first_name" in users[0]
 
 
-def test_get_user_profile(client):
-    user_id = str(uuid4())  # Simulate a valid UUID
-    response = client.get(f"/api/users/{user_id}")
+async def test_get_user_profile(async_client: AsyncClient, created_user_id: str):
+    response = await async_client.get(f"/api/users/{created_user_id}")
     assert response.status_code == 200
     user = response.json()
-    assert user["id"] == user_id
+    assert user["id"] == created_user_id
     assert "first_name" in user
     assert "last_name" in user
 
 
-def test_get_user_profile_not_found(client):
+async def test_get_user_profile_not_found(async_client: AsyncClient):
     user_id = str(uuid4())  # Simulate a non-existent user
-    response = client.get(f"/api/users/{user_id}")
+    response = await async_client.get(f"/api/users/{user_id}")
     assert response.status_code == 404
     assert "detail" in response.json()
-    assert response.json()["detail"] == "User does not exist."
+    assert response.json()["detail"] == f"User with id='{user_id}' does not exist"
 
 
-def test_update_user(client):
-    user_id = str(uuid4())  # Simulate a valid UUID
+async def test_update_user(async_client: AsyncClient, created_user_id: str):
     update_data = {
         "first_name": "John",
         "last_name": "Smith",
@@ -59,31 +50,30 @@ def test_update_user(client):
         "zipcode": 10001,
         "profile_photo": "http://example.com/new_photo.jpg"
     }
-    response = client.put(f"/api/users/{user_id}", data=update_data)
+    response = await async_client.put(f"/api/users/{created_user_id}", data=update_data)
     assert response.status_code == 200
-    user = response.json()
+    user_ = await async_client.get(f"/api/users/{created_user_id}")
+    user = user_.json()
     assert user["first_name"] == "John"
     assert user["last_name"] == "Smith"
     assert user["phone"] == 9876543210
 
 
-def test_update_user_no_data_provided(client):
-    user_id = str(uuid4())  # Simulate a valid UUID
-    response = client.put(f"/api/users/{user_id}", data={})
+async def test_update_user_no_data_provided(async_client: AsyncClient, created_user_id: str):
+    response = await async_client.put(f"/api/users/{created_user_id}", data={})
     assert response.status_code == 400
     assert "detail" in response.json()
-    assert response.json()["detail"] == "No update data provided."
+    assert response.json()["detail"] == "No update data provided"
 
 
-def test_delete_user(client):
-    user_id = str(uuid4())  # Simulate a valid UUID
-    response = client.delete(f"/api/users/{user_id}")
+async def test_delete_user(async_client: AsyncClient, created_user_id: str):
+    response = await async_client.delete(f"/api/users/{created_user_id}")
     assert response.status_code == 204
 
 
-def test_delete_user_not_found(client):
+async def test_delete_user_not_found(async_client: AsyncClient):
     user_id = str(uuid4())  # Simulate a non-existent user
-    response = client.delete(f"/api/users/{user_id}")
+    response = await async_client.delete(f"/api/users/{user_id}")
     assert response.status_code == 404
     assert "detail" in response.json()
-    assert response.json()["detail"] == "User does not exist."
+    assert response.json()["detail"] == f"User with id='{user_id}' does not exist"
